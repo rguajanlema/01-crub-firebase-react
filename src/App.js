d020e7ae0f7751933560e522e9f1c8a74e708370
Roberto <ruidaing@hotmail.com>
2022-03-19 21:08:23 -0500

    Creacion de Home y Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+import Home from "./views/Home";
+import Login from "./views/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [usuario, setUsuario] = React.useState(null);
+
+  return <Container fluid>{usuario ? <Home /> : <Login />}</Container>;
 }
 
 export default App;
