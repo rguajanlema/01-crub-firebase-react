acf6687f1834b58612ce33eec92e1629200b9a7c
Roberto <ruidaing@hotmail.com>
2022-03-19 22:04:46 -0500

    implementacion de inicio de sesion

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import loginEmailPassword from "../functions/loginEmailPassword";
 
 function Login() {
+  async function submitHandler(e) {
+    e.preventDefault();
+    const correo = document.getElementById("formCorreo").value;
+    const contrasena = e.target.formContrasena.value;
+
+    await loginEmailPassword(correo, contrasena);
+  }
+
   return (
     <Container>
       <h1>Iniciar sesion, por favor</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group controlId="formCorreo">
           <Form.Label>Correo</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
