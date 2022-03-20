3286774be15fc4ca009d970b16b5d6b2ac4c8bdc
Roberto <ruidaing@hotmail.com>
2022-03-19 22:18:22 -0500

    inicio de sesion con correo

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import Container from "react-bootstrap/Container";
 import Home from "./views/Home";
 import Login from "./views/Login";
 
+import firebaseApp from "./firebase/credenciales";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth(firebaseApp);
+
 function App() {
   const [usuario, setUsuario] = React.useState(null);
+
+  //se ejecuta cada en cada cambio de estado
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      setUsuario(usuarioFirebase);
+    } else {
+      setUsuario(null);
+    }
+  });
 
   return <Container fluid>{usuario ? <Home /> : <Login />}</Container>;
 }
