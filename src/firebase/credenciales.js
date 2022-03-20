3b730579b9a2af455f7bb6e07d3d0c986af2068e
Roberto <ruidaing@hotmail.com>
2022-03-19 21:38:11 -0500

    firebase credencials

---
+++ src/firebase/credenciales.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7GMxpFrImbDwm55vAT50ZLN3AowZdkcw",
+  authDomain: "crud-firebase-react-7f862.firebaseapp.com",
+  projectId: "crud-firebase-react-7f862",
+  storageBucket: "crud-firebase-react-7f862.appspot.com",
+  messagingSenderId: "765776690847",
+  appId: "1:765776690847:web:44c1cfaaddb44b7443ecaa",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
