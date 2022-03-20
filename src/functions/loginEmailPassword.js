95c8742f96d0212ac494f764bc2edfa76f0d807e
Roberto <ruidaing@hotmail.com>
2022-03-19 21:48:15 -0500

    integracion de loginEmailPassword view

---
+++ src/functions/loginEmailPassword.js
@@ -0,0 +1,11 @@
+import firebaseApp from "../firebase/credenciales";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth(firebaseApp);
+
+//abre la sesion Auth
+async function loginEmailPassword(email, password) {
+  signInWithEmailAndPassword(auth, email, password);
+}
+
+export default loginEmailPassword;
