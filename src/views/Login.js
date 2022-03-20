b09edd2d62a1eeb79a53b37ffb257c9b988dd609
Roberto <ruidaing@hotmail.com>
2022-03-19 21:22:06 -0500

    UI, views Login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  return (
+    <Container>
+      <h1>Iniciar sesion, por favor</h1>
+      <Form>
+        <Form.Group controlId="formCorreo">
+          <Form.Label>Correo</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group controlId="formContrasena">
+          <Form.Label>Contrasena</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Iniciar Sesion
+        </Button>
+      </Form>
+    </Container>
+  );
+}
 
 export default Login;
